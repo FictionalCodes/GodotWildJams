7f8cec3fbdf667e8cc8bdbc46cac46cf49005e9b
Lucy Daniels <lucy.daniels.software@gmail.com>
2025-03-16 19:38:59 +0000

    spawn blobs test

--- March2024-Growing/scripts/BlobInputController.gd
+++ March2024-Growing/scripts/BlobInputController.gd
@@ -1,6 +1,7 @@
-class_name BlobInputController extends Node2D
+class_name BlobInputController extends Area2D
 
 var pickedUp: FreeBlob = null
+@export var blobCreate : PackedScene
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -13,6 +14,18 @@ func _unhandled_input(event: InputEvent) -> void:
 		elif pickedUp != null:
 			pickedUp.drop()
 			pickedUp = null
+			
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+		var created = blobCreate.instantiate() as FreeBlob
+		created.global_position = get_global_mouse_position()
+		get_parent().add_child(created)
+
+
+func _physics_process(delta: float) -> void:
+	if pickedUp == null:
+		return
+	
+
 
 func doPointQuery(mousePos: Vector2) -> CollisionObject2D:
 	var worldSpace = get_world_2d().direct_space_state
